9414e651501e81547056e90464fc6005b48f519f
Felipe Borba <felipe.borba0109@gmail.com>
2024-02-04 14:46:03 -0300

    create CustomDialog

--- app/src/main/java/co/tiagoaguiar/course/instagram/register/view/RegisterPhotoFragment.kt
+++ app/src/main/java/co/tiagoaguiar/course/instagram/register/view/RegisterPhotoFragment.kt
@@ -1,14 +1,30 @@
 package co.tiagoaguiar.course.instagram.register.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import co.tiagoaguiar.course.instagram.R
+import co.tiagoaguiar.course.instagram.common.view.CustomDialog
 
 class RegisterPhotoFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_register_photo, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val customDialog = CustomDialog(requireContext())
+        customDialog.setTitle(R.string.define_photo_profile)
+        customDialog.addButton(R.string.photo, R.string.gallery) {
+            when (it.id) {
+                R.string.photo -> {}
+                R.string.gallery -> {}
+            }
+        }
+        customDialog.show()
     }
 }
