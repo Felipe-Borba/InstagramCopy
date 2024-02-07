85c4e7a6037d4a0b8d5600d329114323d5772c80
Felipe Borba <felipe.borba0109@gmail.com>
2024-02-06 21:27:32 -0300

    add camera fragment

---
+++ app/src/main/java/co/tiagoaguiar/course/instagram/camera/view/CameraFragment.kt
@@ -0,0 +1,24 @@
+package co.tiagoaguiar.course.instagram.camera.view
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import co.tiagoaguiar.course.instagram.R
+import co.tiagoaguiar.course.instagram.common.view.CustomDialog
+
+class CameraFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+}
