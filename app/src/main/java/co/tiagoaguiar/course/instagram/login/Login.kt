d59bdca77696965472d9aea90ad265ce5d81073c
Felipe Borba <felipe.borba0109@gmail.com>
2024-02-08 18:30:19 -0300

    add login presenter and more interfaces

--- app/src/main/java/co/tiagoaguiar/course/instagram/login/Login.kt
+++ app/src/main/java/co/tiagoaguiar/course/instagram/login/Login.kt
@@ -1,11 +1,19 @@
 package co.tiagoaguiar.course.instagram.login
 
+import co.tiagoaguiar.course.instagram.common.base.BasePresenter
+import co.tiagoaguiar.course.instagram.common.base.BaseView
+
 interface Login {
-    interface View {
+    interface View: BaseView<Presenter> {
         fun showProgress(enabled: Boolean)
         fun displayEmailFailure(emailError: Int?)
         fun displayPasswordFailure(passwordError: Int?)
         fun onUserAuthenticated()
         fun onUserUnauthorized()
+    }
+
+    interface Presenter : BasePresenter {
+        fun login(email: String, password: String)
+
     }
 }
--- app/src/main/java/co/tiagoaguiar/course/instagram/login/view/LoginActivity.kt
+++ app/src/main/java/co/tiagoaguiar/course/instagram/login/view/LoginActivity.kt
@@ -7,31 +7,40 @@ import androidx.appcompat.app.AppCompatActivity
 import co.tiagoaguiar.course.instagram.common.util.TxtWatcher
 import co.tiagoaguiar.course.instagram.databinding.ActivityLoginBinding
 import co.tiagoaguiar.course.instagram.login.Login
+import co.tiagoaguiar.course.instagram.login.presentation.LoginPresenter
 
 class LoginActivity : AppCompatActivity(), Login.View {
 
     private lateinit var binding: ActivityLoginBinding
+    override lateinit var presenter: Login.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
+        presenter = LoginPresenter(this)
         setContentView(binding.root)
 
         with(binding) {
             loginEditEmail.addTextChangedListener(watcher)
+            loginEditEmail.addTextChangedListener(TxtWatcher {
+                displayEmailFailure(null)
+            })
+
+
             loginEditPassword.addTextChangedListener(watcher)
+            loginEditPassword.addTextChangedListener(TxtWatcher {
+                displayPasswordFailure(null)
+            })
 
             loginBtmEnter.setOnClickListener {
-
-                Handler(Looper.getMainLooper()).postDelayed({
-                    loginBtmEnter.showProgress(false)
-                }, 2000)
+                presenter.login(loginEditEmail.text.toString(), loginEditPassword.text.toString())
             }
         }
     }
 
     private val watcher = TxtWatcher {
-        binding.loginBtmEnter.isEnabled = it.isNotEmpty()
+        binding.loginBtmEnter.isEnabled = binding.loginEditEmail.text.toString().isNotEmpty()
+                && binding.loginEditPassword.text.toString().isNotEmpty()
     }
 
     override fun showProgress(enabled: Boolean) {
