a27ebaf9ad5ca9097d5dd1e74056c46a28cfbeae
Felipe Borba <felipe.borba0109@gmail.com>
2024-02-20 09:23:20 -0300

    select gallery image and crop

--- app/src/main/java/co/tiagoaguiar/course/instagram/register/view/RegisterActivity.kt
+++ app/src/main/java/co/tiagoaguiar/course/instagram/register/view/RegisterActivity.kt
@@ -1,10 +1,15 @@
 package co.tiagoaguiar.course.instagram.register.view
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import co.tiagoaguiar.course.instagram.R
+import co.tiagoaguiar.course.instagram.common.view.CropperImageFragment
+import co.tiagoaguiar.course.instagram.common.view.CropperImageFragment.Companion.KEY_URI
 import co.tiagoaguiar.course.instagram.databinding.ActivityRegisterBinding
 import co.tiagoaguiar.course.instagram.main.view.MainActivity
 import co.tiagoaguiar.course.instagram.register.view.RegisterWelcomeFragment.Companion.KEY_NAME
@@ -53,6 +58,20 @@ class RegisterActivity : AppCompatActivity(), FragmentAttachListener {
         val intent = Intent(this, MainActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(intent)
+    }
+
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        val fragment = CropperImageFragment().apply {
+            arguments = Bundle().apply {
+                putParcelable(KEY_URI, uri)
+            }
+        }
+        replaceFragment(fragment)
+    }
+
+    override fun goToGalleryScreen() {
+        getContent.launch("image/*")
+
     }
 
     private fun replaceFragment(fragment: Fragment) {
--- app/src/main/java/co/tiagoaguiar/course/instagram/register/view/RegisterPhotoFragment.kt
+++ app/src/main/java/co/tiagoaguiar/course/instagram/register/view/RegisterPhotoFragment.kt
@@ -1,13 +1,21 @@
 package co.tiagoaguiar.course.instagram.register.view
 
 import android.content.Context
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResult
+import androidx.fragment.app.setFragmentResultListener
 import co.tiagoaguiar.course.instagram.R
+import co.tiagoaguiar.course.instagram.common.view.CropperImageFragment
+import co.tiagoaguiar.course.instagram.common.view.CropperImageFragment.Companion.KEY_URI
 import co.tiagoaguiar.course.instagram.common.view.CustomDialog
 import co.tiagoaguiar.course.instagram.databinding.FragmentRegisterPhotoBinding
 
@@ -15,6 +23,16 @@ class RegisterPhotoFragment : Fragment(R.layout.fragment_register_photo) {
 
     private var binding: FragmentRegisterPhotoBinding? = null
     private var fragmentAttachListener: FragmentAttachListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setFragmentResultListener(CropperImageFragment.KEY_RESULT) { requestKey, bundle ->
+            val uri = bundle.getParcelable<Uri>(KEY_URI)
+            onCropImageResult(uri)
+        }
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -28,15 +46,7 @@ class RegisterPhotoFragment : Fragment(R.layout.fragment_register_photo) {
 
                 registerBtmNext.isEnabled = true
                 registerBtmNext.setOnClickListener {
-                    val customDialog = CustomDialog(requireContext())
-                    customDialog.setTitle(R.string.define_photo_profile)
-                    customDialog.addButton(R.string.photo, R.string.gallery) {
-                        when (it.id) {
-                            R.string.photo -> {}
-                            R.string.gallery -> {}
-                        }
-                    }
-                    customDialog.show()
+                    openDialog()
                 }
             }
         }
@@ -46,6 +56,32 @@ class RegisterPhotoFragment : Fragment(R.layout.fragment_register_photo) {
         super.onAttach(context)
         if (context is FragmentAttachListener) {
             fragmentAttachListener = context
+        }
+    }
+
+    private fun openDialog() {
+        val customDialog = CustomDialog(requireContext())
+        customDialog.setTitle(R.string.define_photo_profile)
+        customDialog.addButton(R.string.photo, R.string.gallery) {
+            when (it.id) {
+                R.string.photo -> {}
+                R.string.gallery -> {
+                    fragmentAttachListener?.goToGalleryScreen()
+                }
+            }
+        }
+        customDialog.show()
+    }
+
+    private fun onCropImageResult(uri: Uri?) {
+        if (uri != null) {
+            val bitmap = if (Build.VERSION.SDK_INT >= 28) {
+                val source = ImageDecoder.createSource(requireContext().contentResolver, uri)
+                ImageDecoder.decodeBitmap(source)
+            } else {
+                MediaStore.Images.Media.getBitmap(requireContext().contentResolver, uri)
+            }
+            binding?.registerImgProfile?.setImageBitmap(bitmap)
         }
     }
 
