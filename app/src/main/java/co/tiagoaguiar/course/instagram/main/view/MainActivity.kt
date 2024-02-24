ff5cdb21595c50b472b31dc8488f22330e48d0e7
Felipe Borba <felipe.borba0109@gmail.com>
2024-02-24 14:46:33 -0300

    pre load all screen on bottom navigation

--- app/src/main/java/co/tiagoaguiar/course/instagram/main/view/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/course/instagram/main/view/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
     private lateinit var searchFragment: Fragment
     private lateinit var cameraFragment: Fragment
     private lateinit var profileFragment: Fragment
-    private var currentFragment: Fragment? = null
+    private lateinit var currentFragment: Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,6 +53,15 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         cameraFragment = CameraFragment()
         profileFragment = ProfileFragment()
 
+        currentFragment = homeFragment
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.main_fragment, searchFragment, "3").hide(searchFragment)
+            add(R.id.main_fragment, cameraFragment, "2").hide(cameraFragment)
+            add(R.id.main_fragment, profileFragment, "1").hide(profileFragment)
+            add(R.id.main_fragment, homeFragment, "0")
+            commit()
+        }
+
         binding.mainBottomNav.setOnNavigationItemSelectedListener(this)
         binding.mainBottomNav.selectedItemId = R.id.menu_bottom_home
     }
@@ -61,32 +70,36 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         when (item.itemId) {
             R.id.menu_bottom_home -> {
                 if (currentFragment == homeFragment) return false
+                supportFragmentManager.beginTransaction().hide(currentFragment).show(homeFragment).commit()
                 currentFragment = homeFragment
                 scrollToolbarEnabled(false)
             }
 
             R.id.menu_bottom_search -> {
                 if (currentFragment == searchFragment) return false
+                supportFragmentManager.beginTransaction().hide(currentFragment).show(searchFragment).commit()
                 currentFragment = searchFragment
                 scrollToolbarEnabled(false)
             }
 
             R.id.menu_bottom_add -> {
                 if (currentFragment == cameraFragment) return false
+                supportFragmentManager.beginTransaction().hide(currentFragment).show(cameraFragment).commit()
                 currentFragment = cameraFragment
                 scrollToolbarEnabled(false)
             }
 
             R.id.menu_bottom_profile -> {
                 if (currentFragment == profileFragment) return false
+                supportFragmentManager.beginTransaction().hide(currentFragment).show(profileFragment).commit()
                 currentFragment = profileFragment
                 scrollToolbarEnabled(true)
             }
         }
 
-        currentFragment?.let {
-            replaceFragment(R.id.main_fragment, it)
-        }
+//        currentFragment?.let {
+//            replaceFragment(R.id.main_fragment, it)
+//        }
 
         return true
     }
--- app/src/main/java/co/tiagoaguiar/course/instagram/profile/view/ProfileFragment.kt
+++ app/src/main/java/co/tiagoaguiar/course/instagram/profile/view/ProfileFragment.kt
@@ -1,27 +1,14 @@
 package co.tiagoaguiar.course.instagram.profile.view
 
-import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.Toast
-import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.Adapter
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import co.tiagoaguiar.course.instagram.R
 import co.tiagoaguiar.course.instagram.common.base.BaseFragment
 import co.tiagoaguiar.course.instagram.common.base.DependencyInjector
 import co.tiagoaguiar.course.instagram.common.model.Post
 import co.tiagoaguiar.course.instagram.common.model.UserAuth
-import co.tiagoaguiar.course.instagram.common.view.CustomDialog
 import co.tiagoaguiar.course.instagram.databinding.FragmentProfileBinding
-import co.tiagoaguiar.course.instagram.databinding.FragmentRegisterEmailBinding
 import co.tiagoaguiar.course.instagram.profile.Profile
 import co.tiagoaguiar.course.instagram.profile.presenter.ProfilePresenter
 
